f12c7f707ddc4f8fdc0eaea4e74a1030b0d942bb
stanleydiias <stanleydias17@gmail.com>
2020-10-05 21:51:19 -0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Desculpe " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", parece que meu servidor de e-mail não está respondendo. Por favor, tente novamente mais tarde!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
